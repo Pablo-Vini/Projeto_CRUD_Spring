0dd08425b26610388e5d6d27528b80d4d9703ce0
Pablo Vinícius Gonçalves Ramos <124642141+Pablo-Vini@users.noreply.github.com>
2023-06-16 17:36:10 -0300

    Estudando

---
+++ Estrutura de Dados/src/AcompanhamentoDeProjetos/index.java
@@ -0,0 +1,18 @@
+package AcompanhamentoDeProjetos;
+
+import javax.swing.JFrame;
+
+
+public class index {
+
+	public static void main(String[] args) {
+		Frame labelFrame = new Frame();
+		
+		labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		labelFrame.setSize(260, 180);
+		
+		labelFrame.setVisible(true);
+	}
+
+}
