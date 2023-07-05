78574be9dd6102c061f74616369d0b92ae4877bc
Pablo-Vini <pablovgramos@gmail.com>
2023-07-04 21:05:30 -0300

    Projeto falha

--- CRUD Java/demo/src/main/java/com/projetocrud/demo/entities/Department.java
+++ CRUD Java/demo/src/main/java/com/projetocrud/demo/entities/Department.java
@@ -1,8 +1,19 @@
 package com.projetocrud.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_department")
 public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
 	
 	public Department() {
 		
--- CRUD Java/demo/src/main/java/com/projetocrud/demo/entities/User.java
+++ CRUD Java/demo/src/main/java/com/projetocrud/demo/entities/User.java
@@ -1,10 +1,26 @@
 package com.projetocrud.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	
+	
+	@ManyToOne
+	@JoinColumn(name = "departament_id")
 	private Department department;
 	
 	public User() {
